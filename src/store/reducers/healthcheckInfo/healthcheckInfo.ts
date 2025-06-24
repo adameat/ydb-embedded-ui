4ce8d84ad2afcf5cd8ed17e434157d6292e1ad6c
Alexey Efimov <xeno@prnwatch.com>
2025-06-24 17:09:06 +0700

    Fix healthcheck sorting import

--- src/store/reducers/healthcheckInfo/healthcheckInfo.ts
+++ src/store/reducers/healthcheckInfo/healthcheckInfo.ts
@@ -1,6 +1,7 @@
 import {createSelector} from '@reduxjs/toolkit';
 
-import type {IssueLog, StatusFlag} from '../../../types/api/healthcheck';
+import type {IssueLog} from '../../../types/api/healthcheck';
+import {StatusFlag} from '../../../types/api/healthcheck';
 import type {RootState} from '../../defaultStore';
 import {api} from '../api';
 
